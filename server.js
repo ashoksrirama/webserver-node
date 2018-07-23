cd36b9b53eac48a07883ed10cdb02f5b6b8cfe67
ashok <SreeAbhi@Ashoks-MBP.fios-router.home>
2018-07-23 08:19:10 -0400

    Heroku changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
         error: 'Error fulfilling the request'
     });
 });
-app.listen(3000, () => {
-    console.log('Server is up on 3000 port.');
+app.listen(port, () => {
+    console.log(`Server is up on ${port} port.`);
 });
